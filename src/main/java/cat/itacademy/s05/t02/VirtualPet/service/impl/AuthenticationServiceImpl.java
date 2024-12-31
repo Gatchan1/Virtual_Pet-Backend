95dcad5f1904a50c8ce493f0726ffe7e170483d6
Gatchan1 <r.barrio.f@gmail.com>
2024-12-31 17:15:25 +0100

    improve logging and enable file logging

--- src/main/java/cat/itacademy/s05/t02/VirtualPet/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/cat/itacademy/s05/t02/VirtualPet/service/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package cat.itacademy.s05.t02.VirtualPet.service.impl;
 
 import cat.itacademy.s05.t02.VirtualPet.enums.Role;
+import cat.itacademy.s05.t02.VirtualPet.exception.GlobalExceptionHandler;
 import cat.itacademy.s05.t02.VirtualPet.exception.custom.UserAlreadyExistsException;
 import cat.itacademy.s05.t02.VirtualPet.model.User;
 import cat.itacademy.s05.t02.VirtualPet.dto.AuthenticationRequest;
@@ -9,6 +10,8 @@ import cat.itacademy.s05.t02.VirtualPet.repository.UserRepository;
 import cat.itacademy.s05.t02.VirtualPet.service.AuthenticationService;
 import cat.itacademy.s05.t02.VirtualPet.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,6 +21,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
@@ -37,17 +41,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .role(Role.USER)
                 .build();
         userRepository.save(user);
+        log.info("User {} successfully logged in", user.getUsername());
         return jwtProvider.generateToken(user);
     }
 
     @Override
     public String authenticate(AuthenticationRequest authenticationRequest) { //login
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                authenticationRequest.getUserName(), authenticationRequest.getPassword()
-        ));
         User user = userRepository.findByUserName(authenticationRequest.getUserName())
                 .orElseThrow(() -> new UsernameNotFoundException("User with username " +
                         authenticationRequest.getUserName() + " not found"));
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUserName(), authenticationRequest.getPassword()
+        ));
+        log.info("User {} successfully registered", user.getUsername());
         return jwtProvider.generateToken(user);
     }
 }
